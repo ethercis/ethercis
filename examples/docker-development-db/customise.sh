f93ced71197871c4f09f7e08a7e48333bcea7e25
serefarikan <serefarikan@gmail.com>
2018-02-02 17:04:18 +0000

    upgrade dockerfile and scripts to pg 10.1

--- examples/docker-development-db/customise.sh
+++ examples/docker-development-db/customise.sh
@@ -5,7 +5,7 @@ apt-get install pgxnclient  -y
 apt-get install postgresql-contrib -y
 
 
-apt-get install postgresql-server-dev-9.6  -y
+apt-get install postgresql-server-dev-10  -y
 
 apt-get install ca-certificates -y
 apt-get install build-essential -y
@@ -28,5 +28,5 @@ make USE_PGXS=1 install
 
 su - postgres -c "psql < createdb.sql"
 
-su - postgres -c "/usr/lib/postgresql/9.6/bin/pg_ctl  -D $PGDATA -m fast -w stop"
+su - postgres -c "/usr/lib/postgresql/10/bin/pg_ctl  -D $PGDATA -m fast -w stop"
 
